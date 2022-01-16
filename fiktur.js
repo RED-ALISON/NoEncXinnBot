4ae14dc693787bcf5346322b2be2a7cbddb95521
LordHiroHito <lordhirohito876@gmail.com>
2022-01-16 18:23:02 +0800

    TURU

---
+++ fiktur.js
@@ -0,0 +1,38 @@
+['https://telegra.ph/file/4f7ad4605cc11b1f4e538.mp4', '', ];
+
+                 case 'neko1':
+qute = fs.readFileSync('./img/thumb.jpg') 
+LexxyOFC.sendMessage(from, qute, image, { quoted: flexx, caption: 'NIH LINKNYA: https://www2.zippyshare.com/d/z9dcY6Nr/791216/%5bNekoPoi%5d_Akina_to_Onsen_de_H_Shiyo%5b360P%5d%5bnekopoi.care%5d.mp4\n\nNOTE SIAPKAN TISU'})
+                  break
+                 case 'neko2':
+qute = fs.readFileSync('./img/thumb.jpg') 
+LexxyOFC.sendMessage(from, qute, image, { quoted: flexx, caption: 'NIH LINKNYA: https://www4.zippyshare.com/d/j7PFLGUY/946757/%5bNekoPoi%5d_Tsuma_ga_Kirei_ni_Natta_Wake_-_02_%5b360P%5d_%5bnekopoi.care%5d.mp4\n\nNOTE SIAPKAN TISU'})
+                  break
+                 case 'neko3':
+qute = fs.readFileSync('./img/thumb.jpg') 
+LexxyOFC.sendMessage(from, qute, image, { quoted: flexx, caption: 'NIH LINKNYA: https://www111.zippyshare.com/d/EK5uqIMh/566060/%5bNekoPoi%5d_Megane_no_Megami_-_01_%5b360P%5d_%5bnekopoi.care%5d.mp4\n\nNOTE SIAPKAN TISU'})
+                  break
+                 case 'neko4':
+qute = fs.readFileSync('./img/thumb.jpg') 
+LexxyOFC.sendMessage(from, qute, image, { quoted: flexx, caption: 'NIH LINKNYA: https://www109.zippyshare.com/d/2ebALhhS/463654/%5bNekoPoi%5d_Watashi_ga_Toriko_-_02%5b360P%5d%5bnekopoi.care%5d.mp4\n\nNOTE SIAPKAN TISU'})
+                  break
+                 case 'neko5':
+qute = fs.readFileSync('./img/thumb.jpg') 
+LexxyOFC.sendMessage(from, qute, image, { quoted: flexx, caption: 'NIH LINKNYA: https://www115.zippyshare.com/d/LmzOkRAI/313299/%5bNekoPoi%5d_Chichi-iro_Toiki_-_01_%5b360P%5d%5bnekopoi.care%5d.mp4\n\nNOTE SIAPKAN TISU'})
+                  break
+                 case 'neko6':
+qute = fs.readFileSync('./img/thumb.jpg') 
+LexxyOFC.sendMessage(from, qute, image, { quoted: flexx, caption: 'NIH LINKNYA: https://www89.zippyshare.com/d/SgEHvrJs/42812/%5bNekoPoi%5d_Chichi-iro_Toiki_-_02_%5b360P%5d%5bnekopoi.care%5d.mp4\n\nNOTE SIAPKAN TISU'})
+                  break
+                 case 'neko7':
+qute = fs.readFileSync('./img/thumb.jpg') 
+LexxyOFC.sendMessage(from, qute, image, { quoted: flexx, caption: 'NIH LINKNYA: https://www66.zippyshare.com/d/j0ivbciL/520411/%5bNekoPoi%5d_Onna_Maou_Melissa_no_H_na_Boukenki_-_01_%5b360P%5d_%5bnekopoi.care%5d.mp4\n\nNOTE SIAPKAN TISU'})
+                  break
+                 case 'neko8':
+qute = fs.readFileSync('./img/thumb.jpg') 
+LexxyOFC.sendMessage(from, qute, image, { quoted: flexx, caption: 'NIH LINKNYA: https://www71.zippyshare.com/d/M225YIrR/535656/%5bNekoPoi%5d_Buta_no_Gotoki_Sanzoku_ni_Torawarete_Shojo_wo_Ubawareru_Kyonyuu_Himekishi_Onna_Senshi_-_01%5b360P%5d%5bnekopoi.care%5d.mp4\n\nNOTE SIAPKAN TISU'})
+                  break
+                 case 'neko9':
+qute = fs.readFileSync('./img/thumb.jpg') 
+LexxyOFC.sendMessage(from, qute, image, { quoted: flexx, caption: 'NIH LINKNYA: https://www49.zippyshare.com/d/bdwYjaXS/605790/%5bNekoPoi%5d_Akebi_no_Hana___Maho_-_01_%5b360P%5d_%5b+poi.pro%5d.mp4\n\nNOTE SIAPKAN TISU'})
+                  break
